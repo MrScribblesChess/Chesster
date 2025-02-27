81f5704c4ed9dc816b06613097c1cfaed0fb71ed
mrscribbleschess <mrscribbleschess@gmail.com>
2025-02-27 13:29:20 -0500

    Debugging logs

--- config/testconfig.js
+++ config/testconfig.js
@@ -4,6 +4,10 @@ const UNSTABLE_BOT_ID = 'C016G6T5QTW'
 const UNSTABLE_BOT_LONEWOLF_ID = 'C015V92UJUX'
 
 let config = require('./config.js')
+
+// TODO delete this after debugging
+console.log('config1:', config)
+
 config['watcherBaseURL'] = 'https://lichess.dev/api/stream/games-by-users'
 config['winston']['channel'] = '#modster-logging'
 config['winston']['handleExceptions'] = false
@@ -12,7 +16,7 @@ config['welcome']['channel'] = 'dev-testing-lonewolf'
 
 let heltour = {
     baseEndpoint: 'http://localhost:8000/api/',
-    token: config['heltour']['token']
+    token: config['heltour']['token'],
 }
 let leagues = ['45+45', 'lonewolf', 'blitzbattle', 'chess960']
 config['heltour'] = heltour
@@ -33,9 +37,8 @@ config['leagues']['lonewolf']['scheduling']['channel'] = 'dev-testing-lonewolf'
 config['leagues']['lonewolf']['results']['channel'] = 'dev-testing-lonewolf'
 config['leagues']['lonewolf']['results']['channelId'] = UNSTABLE_BOT_LONEWOLF_ID
 config['leagues']['lonewolf']['gamelinks']['channel'] = 'dev-testing-lonewolf'
-config['leagues']['lonewolf']['gamelinks'][
-    'channelId'
-] = UNSTABLE_BOT_LONEWOLF_ID
+config['leagues']['lonewolf']['gamelinks']['channelId'] =
+    UNSTABLE_BOT_LONEWOLF_ID
 
 config['channelMap'][UNSTABLE_BOT_ID] = '45+45'
 config['channelMap']['dev-testing'] = '45+45'
@@ -45,9 +48,13 @@ config['channelMap'][UNSTABLE_BOT_LONEWOLF_ID] = 'lonewolf'
 
 config['messageForwarding']['channelId'] = 'G3FJXJ0C9'
 config['pingMods'] = {
-    "C016G6T5QTW": [ // #dev-testing
-        "U0164C6FXLK" // @lakinwecker
+    C016G6T5QTW: [
+        // #dev-testing
+        'U0164C6FXLK', // @lakinwecker
     ],
 }
+
+// TODO delete this after debugging
+console.log('config2:', config)
 
 module.exports = config
